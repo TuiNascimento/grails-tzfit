0b5aaee8b3311d275e5206f49a53f7fafc742142
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2022-01-09 03:15:28 -0300

    CardTagLib

---
+++ src/test/groovy/tzfit/CardTagLibSpec.groovy
@@ -0,0 +1,18 @@
+package tzfit
+
+import grails.testing.web.taglib.TagLibUnitTest
+import spock.lang.Specification
+
+class CardTagLibSpec extends Specification implements TagLibUnitTest<CardTagLib> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
