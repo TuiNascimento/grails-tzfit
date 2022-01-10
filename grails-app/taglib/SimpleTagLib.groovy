c1075a6cc3a57ba30422ea45fc711f9ea646f4e5
arthur.nascimento <arthur.nascimento@neomind.com.br>
2022-01-10 20:31:39 -0300

    refactor of sheet edit views

--- grails-app/taglib/SimpleTagLib.groovy
+++ grails-app/taglib/SimpleTagLib.groovy
@@ -8,4 +8,13 @@ class SimpleTagLib {
         }
     }
 
+    def fieldLabel = {attrs, body ->
+        String forEntity = ${attrs.for};
+        //TODO: Implementar tradução
+        String capitalizedForEntity = forEntity.charAt(0).toUpperCase() + forEntity.substring(1);
+        out << "<label for=\"${forEntity}\">${capitalizedForEntity}:"
+        out << "<span class=\"required-indicator\">*</span>"
+        out << "</label>"
+    }
+
 }
